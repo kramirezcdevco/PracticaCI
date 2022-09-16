d3142df86e399a83c67bb265f570403c637f0668
Karen Ramirez <kramirezc@devco.com.co>
2022-09-15 22:53:34 -0500

    Se crea clase de pruebas unitarias para WordCounter

---
+++ src/test/java/org/karenramirez/practicaci/counter/WordCounterDefaultT.java
@@ -0,0 +1,14 @@
+package org.karenramirez.practicaci.counter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WordCounterDefaultTest {
+
+    @Test
+    void mustReturnCorrectSummary()
+    {
+        Assertions.fail();
+    }
+
+}
